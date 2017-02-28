373f3e64d185c01a878c4c90eff7580610725faa
G5Olivieri <glayson.murollo75@gmail.com>
2017-02-28 20:25:21 -0300

    Update vision.cpp

--- Vision/vision.cpp
+++ Vision/vision.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 {
 	if (argc < 2)
 	{
-		std::cout << "Uso: " << argv[0] << "<file_path>\n";
+		std::cout << "Uso: " << argv[0] << " <file_path>\n";
 		return 0;
 	}
 	return 0;
